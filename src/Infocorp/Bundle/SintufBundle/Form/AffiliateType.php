4d0e0a3a503c9c37b1f73e70b6ed3b3480ac5de6
Eduardo Borges <eduardomrb@gmail.com>
2013-09-09 15:21:13 -0400

    Adiciona controller de affiliate (dummy code) e começa AffiliateType (formulario de afiliação)

---
+++ src/Infocorp/Bundle/SintufBundle/Form/AffiliateType.php
@@ -0,0 +1,19 @@
+<?php
+namespace Infocorp\Bundle\SintufBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AffiliateType extends AbstractType
+{
+    public function buildForm(FormBuilderIntrface $formBuilder, array $options)
+    {
+        $formBuilder
+            ->
+    }
+
+    public function getName()
+    {
+        return 'infocorp_type_affiliate';
+    }
+}
