81e5708cbbb07e02c599e360e8ee3f0ae70a2f8e
EduardoMRB <eduardomrb@gmail.com>
2013-09-18 04:49:06 -0400

    Adiciona informações que faltavam em AffiliateType

--- src/Infocorp/Bundle/SintufBundle/Form/AffiliateType.php
+++ src/Infocorp/Bundle/SintufBundle/Form/AffiliateType.php
@@ -3,49 +3,88 @@ namespace Infocorp\Bundle\SintufBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Infocorp\Bundle\SintufBundle\Entity\Affiliate;
 
 class AffiliateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $formBuilder, array $options)
     {
         $formBuilder
-            ->add('registration', 'money', array('label' => 'Matrícula'))
-            ->add('name')
-            ->add('address')
-            ->add('district')
-            ->add('email')
-            ->add('phone')
-            ->add('city')
-            ->add('state')
-            ->add('city')
-            ->add('birthCity')
-            ->add('birthState')
-            ->add('birthDate')
-            ->add('sex')
-            ->add('civilStatus')
-            ->add('associateDate')
-            ->add('filiationFather')
-            ->add('filiationMother')
-            ->add('scholarity')
-            ->add('registrationId')
-            ->add('expeditionOrgan')
-            ->add('expeditionDate')
-            ->add('cpf')
-            ->add('ctps')
-            ->add('pis')
-            ->add('voterTitle')
-            ->add('zone')
-            ->add('section')
-            ->add('level')
-            ->add('class')
-            ->add('pattern')
-            ->add('regime')
-            ->add('admissionDate')
-            ->add('stocking')
-            ->add('bankName')
-            ->add('function')
-            ->add('bankAgency')
-            ->add('bankAccount')
+            ->add('registration', 'text', array('label' => 'Matrícula'))
+            ->add('name', null, array('label' => 'Nome'))
+            ->add('address', null, array('label' => 'Endereço'))
+            ->add('district', null, array('label' => 'Bairro'))
+            ->add('email', 'email', array('label' => 'Email'))
+            ->add('phone', null, array('label' => 'Telefone'))
+            ->add('city', null, array('label' => 'Cidade'))
+            ->add('state', null, array('label' => 'Estado'))
+            ->add('birthCity', null, array('label' => 'Cidade natal'))
+            ->add('birthState', null, array('label' => 'Estado natal'))
+            ->add('birthDate', null, array(
+            	'label' => 'Data de nascimento',
+            	'widget' => 'single_text',
+        	))
+            ->add('sex', 'choice', array(
+            	'choices' => array(
+            		'm' => 'Masculino',
+            		'f' => 'Feminino',
+        		),
+        		'label' => 'Sexo',
+        	))
+            ->add('civilStatus', 'choice', array(
+            	'choices' => array(
+            		'solteiro' => 'Solteiro',
+            		'casado' => 'Casado',
+            		'divorciado' => 'Divorciado',
+            		'viuvo' => 'Viúvo',
+    			),
+    			'label' => 'Estado civil',
+        	))
+            ->add('associateDate', null, array(
+        		'widget' => 'single_text',
+        		'label' => 'Data admissão sócio'
+        	))
+            ->add('filiationFather', null, array(
+            	'label' => 'Pai',
+        	))
+            ->add('filiationMother', null, array(
+            	'label' => 'Mãe',
+    		))
+            ->add('scholarity', 'choice', array(
+            	'choices' => array(
+            		Affiliate::SCHOLARITY_ELEMENTARY_INCOMPLETE => 'Ensino fundamental incompleto',
+            		Affiliate::SCHOLARITY_ELEMENTARY_COMPLETE => 'Ensino fundamental completo',
+            		Affiliate::SCHOLARITY_MEDIUM_INCOMPLETE => 'Ensino médio incompleto',
+            		Affiliate::SCHOLARITY_MEDIUM_COMPLETE => 'Ensino médio completo',
+            		Affiliate::SCHOLARITY_SUPERIOR_INCOMPLETE => 'Ensino superior incompleto',
+            		Affiliate::SCHOLARITY_SUPERIOR_COMPLETE => 'Ensino superior completo',
+        		),
+        		'label' => 'Nível de instrução',
+        	))
+            ->add('registrationId', null, array('label' => 'RG'))
+            ->add('expeditionOrgan', null, array('label' => 'Orgão de expedição'))
+            ->add('expeditionDate', null, array(
+            	'widget' => 'single_text',
+            	'label' => 'Data de expedição',
+        	))
+            ->add('cpf', null, array('label' => 'CPF'))
+            ->add('ctps', null, array('label' => 'CTPS'))
+            ->add('pis', null, array('label' => 'PIS/PASEP'))
+            ->add('voterTitle', null, array('label' => 'Título de eleitor'))
+            ->add('zone', null, array('label' => 'Zona'))
+            ->add('section', null, array('label' => 'Seção'))
+            ->add('level', null, array('label' => 'Nível'))
+            ->add('class', null, array('label' => 'Classe'))
+            ->add('pattern', null, array('label' => 'Padrão'))
+            ->add('admissionDate', null, array(
+            	'widget' => 'single_text',
+            	'label' => 'Data de admissão',
+        	))
+            ->add('stocking', null, array('label' => 'Lotação'))
+            ->add('bankName', null, array('label' => 'Banco'))
+            ->add('function', null, array('label' => 'Cargo/Função'))
+            ->add('bankAgency', null, array('label' => 'Agencia'))
+            ->add('bankAccount', null, array('label' => 'Conta'))
             ->add('dependents', 'collection', array(
             	'type' => new DependentType(),
             	'label' => 'Dependentes',
